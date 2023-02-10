798ad61fe668f95707e1e8a699e708daa4ba7299
Juan Ignacio Bogado <juan.bogado.8@hotmail.com>
2023-02-10 19:57:44 -0300

    added gh-pages and deployed frontend

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { AppRouter } from './router';
 
 export const CalendarApp = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
@@ -8,9 +7,7 @@ import { store } from './store';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
   <Provider store={store}>
     <CalendarApp />
   </Provider>
-  // </StrictMode>
 );
